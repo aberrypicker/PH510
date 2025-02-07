820ada6aaa8ab5efb64399dcd5d6f42e2b043497
aberrypicker <114430428+aberrypicker@users.noreply.github.com>
2025-02-07 12:49:07 +0000

    working jobscript for naive code

--- Assignment 1/assignment_1_slurm.sh
+++ Assignment 1/assignment_1_slurm.sh
@@ -2,7 +2,7 @@
 # Propagate environment variables to the compute node
 #SBATCH --export=ALL
 # Run in the standard partition (queue)
-#SBATCH --partition=teaching
+#SBATCH --partition=teaching-gpu
 # Specify project account
 #SBATCH --account=teaching
 # Distribute processes in round-robin fashion
@@ -18,12 +18,14 @@
 # choose which version to load
 
 module purge
+module load anaconda/python-3.10.9/2023.03
+#module add miniconda/3.12.8
 module load openmpi/gcc-8.5.0/4.1.1
 /opt/software/scripts/job_prologue.sh
 
 
-pylint --extension-pkg-allow-list=mpi4py.MPI assignment_1_naive
+pylint --extension-pkg-allow-list=mpi4py.MPI assignment_1_naive.py
 # Modify the line below to run your program
-mpirun -np $SLURM_NPROCS ./assignment_1_naive
-
+#mpirun -np $SLURM_NPROCS ./assignment_1_naive
+mpiexec -n 8 python3 ./assignment_1_naive.py
 /opt/software/scripts/job_epilogue.sh
