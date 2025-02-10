bdf8d16f9107dce8f15cde1cca69c62ff7ee17cf
aberrypicker <114430428+aberrypicker@users.noreply.github.com>
2025-02-10 15:56:05 +0000

    docstrings expanded, Subclass added
    Spherical Polar coords added as a subclass, beginning defining triangle area

--- Assignment 2/assignment_2.py
+++ Assignment 2/assignment_2.py
@@ -1,26 +1,36 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Feb  7 15:49:21 2025
+Created on Fri Feb  7 15:49:21 2025.
 
 @author: aaron
 """
 import math
 
 class Vector:
-    """An object which represents a Vector using coordinates"""
+    """
+    An object which represents a Vector using coordinates, and can be added,
+    subtracted, dot and cross producted, its magnitude determined, and returned
+    via print
+    """
+    
     def __init__(self, x=0, y=0, z=0): #initialises the Vector and its attributes
         self.x, self.y, self.z = x,y,z
-    def __add__(self, other): #addition of two vectors
+    def __add__(self, other):
+        """For addition of two vectors"""
         return Vector(self.x + other.x, self.y + other.y, self.z + other.z)
-    def __sub__(self, other): #subtraction of two vectors
+    def __sub__(self, other):
+        """ For subtraction of two vectors"""
         return Vector(self.x - other.x, self.y - other.y, self.z - other.z)
-    def __dot__(self, other): #dot product between two vectors
+    def dot(self, other):
+        """To calculate the dot product between two vectors"""
         return self.x * other.x + self.y * other.y + self.z * other.z
-    def __cross__(self, other): #cross product between two vectors
+    def cross(self, other):
+        """To determine the cross product between two vectors"""
         return Vector((self.y * other.z - self.z * other.y),
                 (self.z * other.x - self.x * other.z),
                 (self.x * other.y - self.y *other.x))
-    def __abs__(self): #magnitude of vector
+    def norm(self):
+        """Determining and returning the magnitude of the vector"""
         return math.sqrt(self.x**2 + self.y**2 + self.z**2)
     def __repr__(self):
         return f'Vector[x={self.x}, y={self.y}, z={self.z}]'
@@ -31,25 +41,46 @@ class Vector:
         if isinstance(other, Vector):
             return self.x == other.x and self.y == other.y and self.z == other.z
         return NotImplemented
-
-class Sphere(Vector):
-    """An Object to Represent a Vector in Spherical Coordinates"""
 A = Vector(3,4,5)
-print("Vector A = [", A.x, A.y, A.z, "]")
-print("The Magnitude of Vector A is", Vector.__abs__(A))
+print("Vector A =",A)
+print("The Magnitude of Vector A is", Vector.norm(A))
 
 B = Vector(4,5,6)
-print("B = [", B.x, B.y, B.z, "]")
+print("B = ", B)
 
-C = Vector.__add__(A, B)
-ctest = A+B
+C = A + B
 print("Vector A + Vector B = [", C, "]")
 
-D = Vector.__sub__(A,B)
-print("Vector A - Vector B = [D" , D,"]")
+D = A-B
+print("Vector A - Vector B =" , D)
 
-E = Vector.__dot__(A, B)
+E = Vector.dot(A, B)
 print("The Dot Product of A & B is [", E,"]")
 
-F = Vector.__cross__(A,B)
+F = Vector.cross(A,B)
 print("The Cross product of A & B is [", F, "]")
+
+
+
+class Spherical_Polar(Vector):
+    """An Object to Represent a Vector in Spherical Coordinates"""
+    def __init__(self, r, theta, phi):
+        """New Parameters"""
+        Vector.__init__(self,
+                        r*math.cos(theta)*math.cos(phi),
+                        r*math.cos(theta)*math.sin(phi),
+                        r*math.sin(theta))
+    def r(self):
+        """Positional Parameter to Spherical Polar Vector"""
+        return self.norm()
+    def theta(self):
+        """Returns the Azimuthal Angle"""
+        return math.atan2(self.y, self.x)
+    def phi(self):
+        """Returns the Polar Angle"""
+        return math.asin(self.z/self.norm())
+        # if self.degrees == True:
+        #     self.theta = self.theta * 180/math.pi
+        #     self.phi = self.phi * 180/math.pi
+
+#def Triangle_1(V1, V2, V3):
