c2a1af7f413d04b7a0ec625a3e6ae84c7e43b768
aberrypicker <114430428+aberrypicker@users.noreply.github.com>
2025-03-21 15:37:33 +0000

    Monte Carlo uncertainty
    square root of variance for function uncertainty,

--- Assignment_3/assignment_3_code.py
+++ Assignment_3/assignment_3_code.py
@@ -88,9 +88,9 @@ class MonteCarlo:
 
     def calculations(self):
         """
-        Returns all three desired values at once.
+        Returns all four desired values at once.
         """
-        return self.average()[0], self.integral(), self.variance()
+        return self.average()[0], self.integral(), self.variance(), np.sqrt(self.variance())
 
 class Gaussian:
     """
@@ -127,20 +127,23 @@ class Gaussian:
         term_1 = (1/(self.sigma * np.sqrt(2*np.pi)))
         term_2 = np.exp(np.sum(-(abs(self.x - self.x0))**2, axis = 1)/(2 * self.sigma**2))
         return term_1 * term_2 * np.prod(self.transformation(self.t), axis = 1)
-        
 
 dist_1 = Gaussian(0, 1, 100000000, 1)
 
 MonteGaus_1 = MonteCarlo(dist_1.integral, dist_1, -1, 1)
 print()
-print(MonteGaus_1.calculations())
+print("For a 1D Gaussian Function, the respective average, integral, variance, & uncertainty are: ",
+      f"{MonteGaus_1.calculations()[0]:.4f},", f"{MonteGaus_1.calculations()[1]:.4f},",
+      f"{MonteGaus_1.calculations()[2]:.4f},", f"& {MonteGaus_1.calculations()[3]:.4f},")
 print()
 
 dist_6 = Gaussian(0, 1, 100000000, 6)
 
 MonteGaus_6 = MonteCarlo(dist_6.integral, dist_6, -1, 1)
 print()
-print(MonteGaus_6.calculations())
+print("For a 6D Gaussian Function, the respective average, integral, variance, & uncertainty are: ",
+      f"{MonteGaus_6.calculations()[0]:.4f},", f"{MonteGaus_6.calculations()[1]:.4f},",
+      f"{MonteGaus_6.calculations()[2]:.4f},", f"& {MonteGaus_1.calculations()[3]:.4f},")
 print()
 
 sample_2D = Position(1000, 2)
@@ -149,24 +152,21 @@ sample_4D = Position(1000, 4)
 sample_5D = Position(1000, 5)
 
 Monte_2D = MonteCarlo(sample_2D.r, sample_2D, -1, 1)
-print("For a 2D Monte Carlo, the respective average, integral, and variance are: ",
+print("For a 2D Circle, the respective average, integral, variance, & uncertainty are: ",
       f"{Monte_2D.calculations()[0]:.4f},", f"{Monte_2D.calculations()[1]:.4f},",
-      f"& {Monte_2D.calculations()[2]:.4f}")
+      f"{Monte_2D.calculations()[2]:.4f},", f"& {MonteGaus_1.calculations()[3]:.4f},")
 
 Monte_3D = MonteCarlo(sample_3D.r, sample_3D, -1, 1)
-print("For a 3D Monte Carlo, the respective average, integral, and variance are: ",
+print("For a 3D Sphere, the respective average, integral, variance, & uncertainty are: ",
       f"{Monte_3D.calculations()[0]:.4f},", f"{Monte_3D.calculations()[1]:.4f},",
-      f"& {Monte_3D.calculations()[2]:.4f}")
+      f"{Monte_3D.calculations()[2]:.4f},", f"& {MonteGaus_1.calculations()[3]:.4f},")
 
 Monte_4D = MonteCarlo(sample_4D.r, sample_4D, -1, 1)
-print("For a 4D Monte Carlo, the respective average, integral, and variance are: ",
+print("For a 4D Sphere, the respective average, integral, variance, & uncertainty are: ",
       f"{Monte_4D.calculations()[0]:.4f},", f"{Monte_4D.calculations()[1]:.4f},",
-      f"& {Monte_4D.calculations()[2]:.4f}")
+      f"{Monte_4D.calculations()[2]:.4f},", f"& {MonteGaus_1.calculations()[3]:.4f},")
 
 Monte_5D = MonteCarlo(sample_5D.r, sample_5D, -1, 1)
-print("For a 5D Monte Carlo, the respective average, integral, and variance are: ",
+print("For a 5D HyperSphere, the respective average, integral, variance, & uncertainty are: ",
       f"{Monte_5D.calculations()[0]:.4f},", f"{Monte_5D.calculations()[1]:.4f},",
-      f"& {Monte_5D.calculations()[2]:.4f}")
-
-
-
+      f"{Monte_5D.calculations()[2]:.4f},", f"& {MonteGaus_1.calculations()[3]:.4f},")
