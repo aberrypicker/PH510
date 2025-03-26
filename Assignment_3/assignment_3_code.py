f298716a832c2ca5fccbd57eb041b4d78cc1b6f4
aberrypicker <114430428+aberrypicker@users.noreply.github.com>
2025-03-26 14:21:55 +0000

    class file separation
    For further separation, all three classes are given their own files which are called in the main body of code. Also adding documentation to the main body to guide reader on steps being taken

--- Assignment_3/assignment_3_code.py
+++ Assignment_3/assignment_3_code.py
@@ -5,30 +5,36 @@ Version: Python 3.12.8
 Assignment 3: Monte Carlo system creation.
 
 """
-import assignment_3_classes as cl
+import monte_carlo_class as mc
+import shape_points_class as sp
+import gaussian_class as ga
 
-sample_2D = cl.Position(1000000, 2)
-sample_3D = cl.Position(1000000, 3)
-sample_4D = cl.Position(1000000, 4)
-sample_5D = cl.Position(1000000, 5)
 
-Monte_2D = cl.MonteCarlo(sample_2D.r, sample_2D, -1, 1)
+# Use the Position class to initialise an object defined by a number of samples(rows)
+# and a number of dimensions(columns) for the 4 necessary dimensional tests.
+sample_2D = sp.Position(1000000, 2)
+sample_3D = sp.Position(1000000, 3)
+sample_4D = sp.Position(1000000, 4)
+sample_5D = sp.Position(1000000, 5)
+
+Monte_2D = mc.MonteCarlo(sample_2D.r, sample_2D, -1, 1)
 Monte_2D = Monte_2D.parallel_version()
 
-Monte_3D = cl.MonteCarlo(sample_3D.r, sample_3D, -1, 1)
+Monte_3D = mc.MonteCarlo(sample_3D.r, sample_3D, -1, 1)
 Monte_3D = Monte_3D.parallel_version()
 
-Monte_4D = cl.MonteCarlo(sample_4D.r, sample_4D, -1, 1)
+Monte_4D = mc.MonteCarlo(sample_4D.r, sample_4D, -1, 1)
 Monte_4D = Monte_4D.parallel_version()
 
-Monte_5D = cl.MonteCarlo(sample_5D.r, sample_5D, -1, 1)
+Monte_5D = mc.MonteCarlo(sample_5D.r, sample_5D, -1, 1)
 Monte_5D = Monte_5D.parallel_version()
 
-dist_1 = cl.Gaussian(0, 1, 1000000, 1)
-dist_6 = cl.Gaussian(0, 1, 1000000, 6)
+# use the Gaussian class to initialise class objects
+dist_1 = ga.Gaussian(0, 1, 1000000, 1)
+dist_6 = ga.Gaussian(0, 1, 1000000, 6)
 
-MonteGaus_1 = cl.MonteCarlo(dist_1.integral, dist_1, -1, 1)
+MonteGaus_1 = mc.MonteCarlo(dist_1.integral, dist_1, -1, 1)
 MonteGaus_1 = MonteGaus_1.parallel_version()
 
-MonteGaus_6 = cl.MonteCarlo(dist_6.integral, dist_6, -1, 1)
+MonteGaus_6 = mc.MonteCarlo(dist_6.integral, dist_6, -1, 1)
 MonteGaus_6 = MonteGaus_6.parallel_version()
---
+++ Assignment_3/monte_carlo_class.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+"""
+Version: Python 3.12.8
+
+Licensed and Copyrghted 2025.
+
+This file contains a class which allow Monte Carlo simulations to be ran using random
+number sampling. Each subclass has documentation explaining its' subclasses and processes.
+
+"""
+from mpi4py import MPI
+import numpy as np
+
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+nproc = comm.Get_size()
+delta = 1/nproc
+
+class MonteCarlo:
+    """
+    Approximates a given function by finding the expected value (average), then integrates this
+    average across the limits of the function 'a' and 'b'. Finally, the function's variance (error)
+    is found.
+    """
+    def __init__(self, function, classification, a, b):
+        self.a = a
+        self.b = b
+        self.classification = classification
+        self.function = function
+        self.value = function()
+
+
+    def __str__(self):
+        """
+        Confirms which function the Monte Carlo simulation is approximating
+        """
+        return f"Monte Carlo simulation of the function {self.function}"
+
+
+    def average(self):
+        """
+        Calculates the average value of a given function.
+        """
+        value_2 = self.value**2
+        average = 1/self.classification.n * np.sum(self.value)
+        average_2 = 1/self.classification.n * np.sum(value_2)
+        return average, average_2
+
+    def parallel_version(self):
+        """
+        Combines integral calculation of function, variance of function, and other function
+        calculations into single sub-class.
+        """
+        mean = comm.reduce(self.average()[0], op=MPI.SUM, root=0)
+        mean_squared = comm.reduce(self.average()[1], op=MPI.SUM, root=0)
+
+        if rank==0:
+            integral = ((self.b - self.a)**self.classification.d * mean)/ nproc
+
+            variance_1 = (delta * mean)**2
+            variance_2 = delta * mean_squared
+            variance = 1/self.classification.n * (variance_2 - variance_1)
+            uncertainty = np.sqrt(variance) * ((self.b - self.a)**self.classification.d)
+
+            print(f"Average = {mean * delta}, Integral = {integral},",
+            f"Variance = {variance}")
+            print(f"The {self.classification.d}D Integral is {integral:.4f} ± {uncertainty:.4f}",
+            f"units**{self.classification.d}")
+            print()
+            return integral
+        return None
+
