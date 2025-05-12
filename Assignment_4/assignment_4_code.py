65237cd6ff786b97dfb0627161ecfcfa03f0af3f
aberrypicker <114430428+aberrypicker@users.noreply.github.com>
2025-05-12 13:14:43 +0100

    Pyhton file for code, importing numpy, MPI, time

---
+++ Assignment_4/assignment_4_code.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Version: Python 3.12.8
+
+Copyright 2025: Aaron Berryman. Licensed under MIT license.
+
+Assignment 4: Using Green's Function and Random Walks to solve Poisson's Equation
+			  for multiple examples.
+
+"""
+import time
+from mpi4py import MPI
+import numpy as np
+import monte_carlo_class as mc
+
+
+
