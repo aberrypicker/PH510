2c51d8ac87b4eb84a7fb4092d8b39803f244d630
aberrypicker <114430428+aberrypicker@users.noreply.github.com>
2025-05-12 13:44:50 +0100

    defining self + boundary conditions
    defines the poisson grid as a class, and setting the boundary conditions using the start and end columns, and top and bottom rows of the grid

--- Assignment_4/assignment_4_code.py
+++ Assignment_4/assignment_4_code.py
@@ -14,4 +14,62 @@ import numpy as np
 import monte_carlo_class as mc
 
 
+class Poisson_Grid:
+    """
+    Grid thing
+    """
+    def __init__(self, length, n_points):
+        self.l = length
+        self.n = n_points
+        self.h = self.l / (self.n - 1)
+        self.phi = np.random.uniform(0, 1000, (self.n, self.n))
+        self.f = np.zeros((self.n, self.n))
+        self.fixed_potential = set()
+        self.fixed_charge = set()
 
+    def grid_potential(self, x, y, potential):
+        """
+        Looks like volatge but isnt voltage, why is the unit a V fissics mfckers
+        """
+        if 0 <= x < self.n and 0 <= y < self.n:
+            self.phi[x,y] = potential
+            self.fixed_potential.add((x,y))
+        else:
+            raise ValueError(f"({x}, {y}) Coordinates not in Grid.")
+        return self.phi
+
+    def boundary_condition(self, bc_type):
+        """
+        Fucntion behaviviour at edge.
+        """
+        if bc_type == 'Q4-a':
+            self.phi[-1,:] = 1
+            self.phi[0,:] = 1
+            self.phi[:,0] = 1
+            self.phi[:,-1] = 1
+        elif bc_type == 'Q4-b':
+            self.phi[-1,:] = 1
+            self.phi[0,:] = 1
+            self.phi[:,0] = -1
+            self.phi[:,-1] = -1
+        elif bc_type == 'Q4-c':
+            self.phi[-1,:] = 2
+            self.phi[0,:] = 0
+            self.phi[:,0] = 2
+            self.phi[:,-1] = 4
+        else:
+            raise ValueError(f"Boundary Condition not recognised: {bc_type}")
+
+        for i in range(self.n):
+            self.fixed_potential.add((self.n - 1, i))
+            self.fixed_potential.add((0, i))
+            self.fixed_potential.add((i, 0))
+            self.fixed_potential.add((i, self.n - 1))
+
+        return self.phi
+
+def relax/overrelax
+
+boundary check
+
+grid plot
